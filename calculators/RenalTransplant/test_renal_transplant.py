44614a4fc4c4cfb7e303e1ff024602e1b54d3847
uditsharma16 <uditsharma16@yahoo.com>
2020-11-30 16:08:37 +1300

    Updating the test suite

--- calculators/RenalTransplant/test_renal_transplant.py
+++ calculators/RenalTransplant/test_renal_transplant.py
@@ -1,14 +1,19 @@
 import pytest
+import pandas as pd
+import os
 import calculator_renal_transplant as cal
 import validation_renal_transplant as val
 import fetch_data_renal_transplant as fd
 
+path = os.getcwd()
+testfile = pd.read_csv(path+r'\calculators\RenalTransplant\test_suite_data_renal_transplant.csv')
+
 
 class TestCalculator:
     def test_score_albumin(self):
-        albumin_test_data=[24, 25, 27, 28, 31, 33, 35, 38, 39, 40, 41, 45]
-        albumin_test_values=[9, 7, 7, 6, 6, 5, 5, 4, 4, 3, 3, 0]
-        for i,x in enumerate(albumin_test_data):
+        albumin_test_data = list(testfile.albumin_data)
+        albumin_test_values = list(testfile.albumin_test)
+        for i in range(len(albumin_test_data)):
             assert cal.score_albumin(albumin_test_data[i]) == albumin_test_values[i], "albumin is calculated wrong"
 
 
@@ -17,32 +22,57 @@ class TestCalculator:
 
 
     def test_score_bmi(self):
-        assert cal.score_bmi(20.4) == 1, "BMI score doesn't match"
+        bmi_test_data = list(testfile.bmi_data)
+        bmi_test_values = list(testfile.bmi_test)
+        for i in range(len(bmi_test_data)):
+            assert cal.score_bmi(bmi_test_data[i]) == bmi_test_values[i], "BMI score doesn't match"
 
 
     def test_score_cause(self):
-        assert cal.score_cause('Diabetes') == 3, "Score of cause doesn't match"
+        cause_test_data = list(testfile.cause_data)
+        cause_test_values = list(testfile.cause_test)
+        for i in range(len(cause_test_data)):
+            assert cal.score_cause(cause_test_data[i]) == cause_test_values[i], "Score of cause doesn't match"
+
 
     def test_score_history(self):
-        assert cal.score_history('Yes','Employed') == -2, "Score history doesn't match"
+        history_diagnosis_data = list(testfile.history_diagnosis_data)
+        history_result_data = list(testfile.history_result_data)
+        history_test_data = list(testfile.history_test)
+        for i in range(len(testfile.history_diagnosis_data)):
+            assert cal.score_history(history_result_data[i], history_diagnosis_data[i]) == \
+                   history_test_data[i], "Score history doesn't match"
+
 
     def test_score_age(self):
-        age_data = [29, 31, 35, 37.5, 40, 42.5, 45, 46.8, 47, 50.3, 51, 53.7, 55, 57, 58.6, 60.7, 62.3, 65.3,65.4]
-        age_value = [0, 5, 5, 8, 8, 11, 11, 13, 13, 15, 15, 17, 17, 18, 18, 21, 21, 28, 28]
-        for i in range(len(age_data)):
-            assert cal.score_age(age_data[i]) == age_value[i], "Age value doesn't match for" + age_data[i]
+        age_test_data = list(testfile.age_data)
+        age_test_value = list(testfile.age_test)
+        for i in range(len(age_test_data)):
+            assert cal.score_age(age_test_data[i]) == age_test_value[i], "Age value doesn't match"
 
 
     def test_score_ethnicity(self):
-        ethinicity_data=['European', 'NZ European', 'Pacific Islander', 'Asian', 'Maori', 'Other']
-        ethinicity_score=[0, 0, -4, -4, -4, -4]
-        for i in range(len(ethinicity_data)):
-            assert cal.score_ethnicity(ethinicity_data[i]) == ethinicity_score[i], "Wrong Ethnicity score" + \
-                                                                                   ethinicity_data[i]
-            #     def test_score_first_rrt():
-#     def test_score_timefrom_frtt():
-    def test_adjust_transplantation(self):
-        assert cal.adjust_transplantation() == -26
+        ethinicity_test_data = list(testfile.ethnicity_data)
+        ethinicity_test_value = list(testfile.ethnicity_test)
+        for i in range(len(ethinicity_test_data)):
+            assert cal.score_ethnicity(ethinicity_test_data[i]) == ethinicity_test_value[i], "Wrong Ethnicity score" + \
+                                                                                   ethinicity_test_data[i]
+
+
+    def test_score_first_rrt(self):
+        first_rrt_test_data = list(testfile.first_rrt_data)
+        first_rrt_test_value = list(testfile.first_rrt_test)
+        for i in range(len(first_rrt_test_data)):
+            assert cal.score_first_rrt(first_rrt_test_data[i]) == first_rrt_test_value[i], "The first rrt score " \
+                                                                                           "doesn't match"
+
+
+    def test_score_timefrom_frtt(self):
+        timefrom_frtt_test_data = list(testfile.timefrom_frtt_data)
+        timefrom_frtt_test_value = list(testfile.timefrom_frtt_test)
+        for i in range(len(timefrom_frtt_test_data)):
+            assert cal.score_timefrom_frtt(timefrom_frtt_test_data[i]) == timefrom_frtt_test_value[i]
+
     # def test_calculate(self):
     #     assert
 #
