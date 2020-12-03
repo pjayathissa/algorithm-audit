d0c4d4d6abfd20f9dbcf887fe9ee9014dd24a883
uditsharma16 <uditsharma16@yahoo.com>
2020-12-03 15:12:04 +1300

    Updating the validation function
    Dividing the validation function into simpler functions

--- calculators/RenalTransplant/validation_renal_transplant.py
+++ calculators/RenalTransplant/validation_renal_transplant.py
@@ -3,39 +3,44 @@ import pandas as pd
 import numpy as np
 
 
-# function to create the audit report
+# main validation function
 def validation(numeric_results_df):
+    validation_report = []
     # validate bmi calculation
-    result_df_bmi = pd.DataFrame()
-    result_df_bmi['Live Value'] = numeric_results_df['bmis']
-    result_df_bmi['Column Name'] = 'BMI'
-    result_df_bmi['Audit Value'] = numeric_results_df.apply(lambda x: cal.calc_bmi(x['Height'], x['Weight']), axis=1)
-    result_df_bmi['Result'] = result_df_bmi.apply(lambda x: validate(x['Column Name'], x['Live Value'],
-                                                                     x['Audit Value']), axis=1)
+    bmi_audit_value = numeric_results_df.apply(lambda x: cal.calc_bmi(x['Height'], x['Weight']), axis=1)
+    validation_report.append(check_column('BMI', numeric_results_df['bmis'], bmi_audit_value, 'Renal Transplant'))
 
     # validate renal score
-    result_df_renal = pd.DataFrame()
-    result_df_renal['Live Value'] = numeric_results_df['Survival_Factor']
-    result_df_renal['Column Name'] = 'Renal Transplant Survival Score'
-    result_df_renal['Audit Value'] = numeric_results_df.apply(
+    survival_score_audit_value = numeric_results_df.apply(
         lambda x: cal.calculate(height=x['Height'], weight=x['Weight'], date_accepted=x['dateAccepted'],
                                 date_first_rrt=x['dateFirstRRT'], date_referred_to_txCtr=x['dateReferredtoTxCtr'],
                                 albumin=x['Albumin'], cause=x['Cause'], age=x['Age'],
                                 ethinic_group=x['EthnicGroup'], copd=x['COPD'], nonambulatory=x['Nonambulatory'],
                                 chf=x['CHF'], insulin=x['Insulin'], cad=x['CAD'], pvd=x['PVD'], cvd=x['CVD'],
                                 ht=x['HT'], smoker_current=x['SmokerCurrent'], employed=x['Employed']), axis=1)
+    validation_report.append(check_column('Survival Factor', numeric_results_df['Survival_Factor'],
+                                          survival_score_audit_value, 'Renal Transplant'))
 
-    result_df_renal['Result'] = result_df_renal.apply(lambda x: validate(x['Column Name'], x['Live Value'],
-                                                                         x['Audit Value']), axis=1)
-
-    validation_report = pd.concat([result_df_renal, result_df_bmi])
-    validation_report['Calculator Name'] = 'Renal Transplant Calculator'
+    # joining the results into a single data frame
+    validation_report = pd.concat(validation_report)
     return validation_report.dropna(subset=['Result'])
 
 
+# function to create the validation data frame
+def check_column(column_name, live_value, audit_value, calculator_name):
+    result_df = pd.DataFrame()
+    result_df['Live Value'] = live_value
+    result_df['Column Name'] = column_name
+    result_df['Calculator Name'] = calculator_name
+    result_df['Audit Value'] = audit_value
+    result_df['Result'] = result_df.apply(lambda x: validate(x['Column Name'], x['Live Value'],
+                                                             x['Audit Value']), axis=1)
+    return result_df
+
+
 # function to validate the values
-def validate(target_column, acutal_value, audit_value):
-    if acutal_value != audit_value:
+def validate(target_column, actual_value, audit_value):
+    if actual_value != audit_value:
         return "The " + target_column + " live value doesn't match audit value"
     else:
         return np.nan
