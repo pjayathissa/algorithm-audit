048bda9e1ca8a040639986b92df42098d75277ff
uditsharma16 <uditsharma16@yahoo.com>
2020-12-10 13:42:01 +1300

    Adding the CrCl calculator

--- calculators/CrCl/calculator_crcl.py
+++ calculators/CrCl/calculator_crcl.py
@@ -1,8 +1,6 @@
 def calculate(age, weight, serum, gender):
-    crcl_score = (140-age) * weight / (serum*72)
+    crcl_score = (140 - age) * weight / (serum * 72)
     if gender == 'female':
         crcl_score = crcl_score * .85
 
     return crcl_score
-
-
