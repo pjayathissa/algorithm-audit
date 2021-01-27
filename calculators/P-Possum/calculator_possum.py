13636f99a2021ee1cdcb7174c9059e00f0dd6287
uditsharma16 <uditsharma16@yahoo.com>
2021-01-27 16:10:57 +1300

    Adding P-Possum calculator

---
+++ calculators/P-Possum/calculator_possum.py
@@ -0,0 +1,9 @@
+import math
+
+
+def calculate(ps, os):
+
+    x = -7.04 + (0.13*ps) + (0.16*os)
+    mortality_risk = 1/(1+math.exp(-x))
+    x1 = 
+    return 0
---
+++ calculators/P-Possum/test_possum.py
@@ -0,0 +1,36 @@
+# import pytest
+# import calculator_cha2ds2 as cal
+# import validation_cha2ds2 as val
+# import fetch_data_cha2ds2 as fd
+
+
+class TestCalculator:
+    def test_calculate(self):
+        assert True
+
+
+class TestFetchData:
+    def test_fetch_data(self):
+        assert True
+
+    def test_create_connection(self):
+        assert True
+
+    def test_query_data(self):
+        assert True
+
+    def test_clean_data(self):
+        assert True
+
+
+class TestMain:
+    def test_renal_transplant(self):
+        assert True
+
+
+class TestValidation:
+    def test_validate(self):
+        assert True
+
+    def test_validation(self):
+        assert True
